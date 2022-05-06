ee43165a434605de076aa02f68b112ec45710b1c
Brian Johnson <johnson8727@gmail.com>
2022-05-06 16:50:36 -0400

    made some changes to the background color on the body tag. Also added styled-components package to project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 function App() {
   return (
-    <div className="App">
-      These are my React Starter Files Shift the overall look and feel by adding
-      these wonderful touches to furniture in your home Ever been in a room and
-      felt like something was missing? Perhaps it felt slightly bare and
-      uninviting. I’ve got some simple tips to help you make any room feel
-      complete. Michelle Appleton 28 Jun 2020 Share
-      <div class="attribution">
-        Challenge by{" "}
-        <a
-          href="https://www.frontendmentor.io?ref=challenge"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#myid">Your Name Here</a>.
-      </div>
+    <div className="grid__container">
+      <main className="wrapper">
+        <article>
+          <h1 className="sr-only">Article Preview Component</h1>
+          <h2>
+            Shift the overall look and feel by adding these wonderful touches to
+            furniture in your home
+          </h2>
+          <p>
+            Ever been in a room and felt like something was missing? Perhaps it
+            felt slightly bare and uninviting. I’ve got some simple tips to help
+            you make any room feel complete.
+          </p>
+          <div>
+          <h2>Michelle Appleton</h2>
+          <p>28 Jun 2020 Share</p>
+          </div>
+          
+        </article>
+        <footer className="attribution">
+          Challenge by
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#myid">Brian Johnson</a>.
+        </footer>
+      </main>
     </div>
   );
 }
